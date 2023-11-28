388a926ec87745ae64083126b4cc1d33bbde5147
rahulfordev <rahulforsms@gmail.com>
2023-11-28 17:55:42 +0600

    spinner & error message add

--- src/components/CountryDetails/countries/Countries.jsx
+++ src/components/CountryDetails/countries/Countries.jsx
@@ -2,21 +2,34 @@ import { useState, useEffect } from "react";
 import Country from "../country/Country";
 import Search from "../search/Search";
 import "./country.css";
+import spinner from "../../../assets/Spinner.gif";
 
 const Countries = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState(countries);
   const url = "https://restcountries.com/v3.1/all";
 
+  const fetchData = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+      setCountries(data);
+      setFilteredCountries(data);
+      setIsLoading(false);
+      setError(null);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error);
+    }
+  };
+
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        data.sort((a, b) => a.name.common.localeCompare(b.name.common));
-        setCountries(data);
-        setFilteredCountries(data);
-      });
-  }, [setCountries]);
+    fetchData(url);
+  }, []);
 
   // search countrys
   const handleSearch = (searchValue) => {
@@ -32,8 +45,15 @@ const Countries = () => {
       <h1 style={{ fontSize: "2rem" }}>List of Countries</h1>
       <Search onSearch={handleSearch} />
       <div className="countries">
+        {error && <h2>{error.message}</h2>}
+        {isLoading && (
+          <img
+            style={{ textAlign: "center", marginInline: "auto" }}
+            src={spinner}
+            alt="this is spinner"
+          />
+        )}
         {filteredCountries.map((country, index) => (
-          // console.log(country)
           <Country key={index} country={country} />
         ))}
       </div>
